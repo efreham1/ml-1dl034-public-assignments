c97dac7a4794c911c66baa33458c00c9fd0cd874
viwa3399 <viktor.wallsten.3399@student.uu.se>
2024-02-28 18:56:13 +0100

    rf 94.2% acc

--- Project/rf.py
+++ Project/rf.py
@@ -5,20 +5,16 @@ import seaborn as sns
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import GenericUnivariateSelect
+from sklearn.model_selection import RandomizedSearchCV
 
-
-
-
-# svm # Dump-truck jew-jesus
-# knn # giga fredde
-# RandomForest # wallenstam
-# DecisionTree # Ebitch
-# Naive Bayes # Mucus
 
 hyper_params= {
-    "n_estimators": np.arange(90, 110),
-    "max_depth": np.arange(23, 27),
-    "min_samples_split": np.arange(1, 5),
+    "n_estimators": np.arange(95, 110, dtype=int),
+    "max_depth": [None, 10, 20, 30, 40, 50],
+    "min_samples_split": [2, 5, 10, 15, 100],
+    "min_samples_leaf":  [1, 2, 4, 6, 8],
+    "max_features": ['sqrt', 'log2', None],
+    "bootstrap":[True]
 }
 
 df = pd.read_csv('train_dataset.csv')
@@ -46,29 +42,19 @@ plt.savefig(fname="class_distribution")
 
 target = df.pop(' Forest Cover Type Classes')
 features = df
-#features = GenericUnivariateSelect(mode='fdr').fit_transform(features, target)
+features = GenericUnivariateSelect(mode='fdr').fit_transform(features, target)
 
-#features = pd.DataFrame(features)
+features = pd.DataFrame(features)
 
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=69)
 
 
-
-RF_clf = RandomForestClassifier(max_depth=25, random_state=42)
+RF_clf = RandomForestClassifier(bootstrap= True, random_state=42)
 RF_clf.fit(X_train, y_train)
 
-#grid_search = RandomizedSearchCV(RF_clf,hyper_params,n_iter=100, n_jobs=-1)
-#grid_search.fit(X_train,y_train)
 
 feature_scores = pd.Series(RF_clf.feature_importances_, index = X_train.columns).sort_values(ascending= False)
 print(f"number cof columns before drop:", {len(X_train.columns)})
-'''
-# Drops 25 columns, when score < 0.005
-for attribute, score in feature_scores.items():
-    if(score < 0.005):
-        df = df.drop(attribute,axis = 1)
-        # print(f"Dropped column: {attribute}")
-'''
 
 # Identify features to drop (threshold can be adjusted)
 threshold = 0.001
@@ -80,16 +66,14 @@ X_test_new = X_test.drop(features_to_drop, axis=1)
 
 print(f"number cof columns before drop after drop:", {len(X_train_new.columns)})
 
-RF_clf.fit(X_train_new, y_train)
+grid_search = RandomizedSearchCV(RF_clf,hyper_params,n_iter=5, verbose=2, cv=3)
 
-# Evaluation
-accuracy = RF_clf.score(X_test_new, y_test)
+#RF_clf.fit(X_train_new, y_train)
+grid_search.fit(X_train_new,y_train)
 
 
-#y_pred_grid = grid_search.predict(X_test)
-y_pred = RF_clf.predict(X_test_new)
+#print("accuracy not grid:",RF_clf.score(X_test_new, y_test))
+print("accuracy with grid",grid_search.score(X_test_new, y_test))
+print(grid_search.best_params_)
 
-print("accuracy not grid:",accuracy)
-#print("accuracy with grid",grid_search.score(X_test, y_test))
-#print(grid_search.best_params_)
-
+#accuracy with grid 0.9421346244934478 {'n_estimators': 98, 'min_samples_split': 2, 'max_depth': 26}
