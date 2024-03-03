9a1be37dc683c4438cb5e58e0c451753ddabbc79
MarcusNyfjall <nyfjallmarcus@gmail.com>
2024-03-03 11:27:05 +0100

    NB.py klar med gridsearchcv med 64% accuracy

--- Project/NB.py
+++ Project/NB.py
@@ -6,28 +6,23 @@ import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_selection import GenericUnivariateSelect
 
+df = pd.read_csv('train_dataset.csv')
 
-df = pd.read_csv('data.csv')
-
-target = df.pop('Cover_Type')
+target = df.pop(' Forest Cover Type Classes')
 features = df
-#features = GenericUnivariateSelect(mode='fdr').fit_transform(features, target)
 
 X_train,X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=69)
 
-hyper_params = {'var_smoothing': np.linspace(1e-11, 1, 2000)}
+hyper_params = {'var_smoothing': np.linspace(1e-11, 1, 50)}
 
-clf = GaussianNB(var_smoothing=0.010505252636208103)
+clf = GaussianNB()
 
-#grid_search = GridSearchCV(clf, hyper_params, n_jobs=-1)
-#grid_search.fit(X_train, y_train)
-
-#best_params = grid_search.best_params_
-#best_score = grid_search.best_score_
-
-clf = clf.fit(X_train, y_train)
-best_score = clf.score(X_test, y_test)
+grid_search = GridSearchCV(clf, hyper_params, n_jobs=-1)
+grid_search.fit(X_train, y_train)
 
 
-#print("Best hyperparameters:", best_params)
+best_params = grid_search.best_params_
+best_score = grid_search.best_score_
+
+print("Best hyperparameters:", best_params)
 print("Best cross-validation score:", best_score)
