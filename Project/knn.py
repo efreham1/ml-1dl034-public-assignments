f647002904345e311f31807c43d3825faeb6443b
efreham1 <hammarberg83@gmail.com>
2024-03-05 10:55:03 +0100

    grejs

--- Project/knn.py
+++ Project/knn.py
@@ -11,27 +11,28 @@ from sklearn.neighbors import KNeighborsClassifier
 
 hyper_params = {
     "algorithm": ['ball_tree', 'kd_tree'],
-    "leaf_size": np.arange(1, 10),
+    "weights": ['uniform', 'distance'],
+    "leaf_size": np.arange(1, 15),
     "p": np.arange(1, 10),
-    "n_neighbors": np.arange(5, 20)
+    "n_neighbors": np.arange(4, 20)
 }
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('train_dataset.csv')
 
-target = df.pop('Cover_Type')
+target = df.pop(' Forest Cover Type Classes')
 features = df
 
 scaler = preprocessing.StandardScaler()
 
-for col in ['Elevation','Aspect','Slope','R_Hydrology','Z_Hydrology','R_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','R_Fire_Points']:
+for col in ["Elevation (meters)", " Aspect (azimuth)", " Slope (degrees)", " Horizontal_Distance_To_Hydrology (meters)", " Vertical_Distance_To_Hydrology (meters)", " Horizontal_Distance_To_Roadways(meters)", " Hillshade_9am (0-255)", " Hillshade_Noon (0-255)", " Hillshade_3pm (0-255)", " Horizontal_Distance_To_Fire_Points (meters)"]:
     features[col] = pd.DataFrame(scaler.fit_transform(pd.DataFrame(features[col])),columns=[col])
 
 features = GenericUnivariateSelect(mode='fdr').fit_transform(features, target)
 
 X_train,X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=69)
-clf = KNeighborsClassifier(7, weights='distance', algorithm='ball_tree', leaf_size= 3, p= 1)
+clf = KNeighborsClassifier(n_jobs=-1)
 
-#clf = RandomizedSearchCV(clf, hyper_params, n_iter=150, n_jobs=-1)
+#clf = RandomizedSearchCV(clf, hyper_params, n_iter=10, n_jobs=-1)
 #clf = GridSearchCV(clf, hyper_params, n_jobs=-1)
 
 clf.fit(X_train, y_train)
